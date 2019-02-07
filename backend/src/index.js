5bad719f7e110e0dd994d8aff519c26bb8fab626
Andre Marcelino <marcelinoandre@gmail.com>
2019-02-06 23:52:15 -0200

    Backene e frontend ok 3 aula

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,10 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
 
-mongoose.connect('mongodb://curso-react:123mudar@ds121415.mlab.com:21415/curso-react', {
+mongoose.connect('mongodb://<user>:<passworld>@ds121415.mlab.com:21415/curso-react', {
     useNewUrlParser: true
 });
+
 
 app.use(cors());
 app.use((req, res, next)=>{
